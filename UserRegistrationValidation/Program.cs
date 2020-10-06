711808013f41054246420c83915fba5e79342c62
Senendra <senendra.007@gmail.com>
2020-10-06 12:03:07 +0530

    [Senendra] Add: Validating User First Name

---
+++ UserRegistrationValidation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UserRegistrationValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string firstName = "senendra" ;
+            Console.WriteLine(firstName);
+            Validation check = new Validation();
+            bool result = check.ValidatingFirstName(firstName);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ UserRegistrationValidation/Validation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace UserRegistrationValidation
+{
+    class Validation
+    {
+        public string regrexFirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ValidatingFirstName( string firstName)
+        {
+            return Regex.IsMatch(firstName, regrexFirstName);
+        }
+    }
+}
