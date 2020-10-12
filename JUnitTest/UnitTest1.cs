d8e8c611a7b8001a08eb48d813e652326bf725a9
Senendra <senendra.007@gmail.com>
2020-10-13 00:56:25 +0530

    [Senendra] Refactor: JUnit testing to Validate list of email Id's

--- JUnitTest/UnitTest1.cs
+++ JUnitTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Data;
 using UserRegistrationValidation;
 namespace JUnitTest
 {
@@ -118,6 +119,25 @@ namespace JUnitTest
             bool passwordResult = test.ValidatingPassword(password);
            //Assert
             Assert.IsFalse(passwordResult);
+        }
+        [TestMethod]
+        [DataRow("abc@yahoo.com")]
+        [DataRow("abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com")]
+        [DataRow("abc111@abc.com")]
+        [DataRow("abc.100@abc.com.au")]
+        [DataRow("abc-100@abc.net")]
+        [DataRow("abc@1.com")]
+        [DataRow("abc@gmail.com.com")]
+        [DataRow("abc+100@gmail.com")]
+        public void JUnitParametrisedTest(string email)
+        {
+            //Arrange
+            Validation test = new Validation();
+            //Act
+            bool emailResult = test.ValidatingEmail(email);
+            //Assert
+            Assert.IsTrue(emailResult);
         }
     }
 }
